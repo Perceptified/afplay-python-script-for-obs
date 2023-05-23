c9a160832859b0281abbc7b29a9e1ce71d64d4ba
Markus Aigner <perceptified@gmail.com>
2023-05-23 23:39:20 +0200

    first commit

---
+++ afplay-obs-helper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#
+# Script which uses afplay to play audio files, also writes the track title and artist name out
+# to a text file which then can be used in OBS to display that information on stream.
+# Copyright(left) : Markus Aigner 2023 (perceptified@gmail.com)
+# 
+
+import sys
+
+directory=sys.argv[0]
+if(sys.argv[1]==""):
+    volume=0.1
+else:
+    volume=sys.argv[1]
+for i in range(1, n):
+    print(sys.argv[i], end = " ")
+
